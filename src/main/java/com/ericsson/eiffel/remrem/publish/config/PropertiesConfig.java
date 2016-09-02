162129833eee81f774c1e0fdd39c6f2322e7547e
Vali (Vasile Baluta) <vasile.baluta@ericsson.com>
2016-09-02 14:57:45 +0200

    change default message bus to localhost

---
+++ src/main/java/com/ericsson/eiffel/remrem/publish/config/PropertiesConfig.java
@@ -0,0 +1,6 @@
+package com.ericsson.eiffel.remrem.publish.config;
+
+public class PropertiesConfig {
+	public static final String MESSAGE_BUSS_HOST = "com.ericsson.eiffel.remrem.publish.messagebus.host";
+	public static final String EXCHANGE_NAME = "com.ericsson.eiffel.remrem.publish.exchange.name";
+}
