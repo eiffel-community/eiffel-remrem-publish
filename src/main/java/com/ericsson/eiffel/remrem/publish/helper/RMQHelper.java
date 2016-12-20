e340665ec6ae1745ca41f75779162e95a871367f
xlakdas <lakshmi.dasari@tcs.com>
2016-12-20 18:58:11 +0530

    proper java format for the code

--- src/main/java/com/ericsson/eiffel/remrem/publish/helper/RMQHelper.java
+++ src/main/java/com/ericsson/eiffel/remrem/publish/helper/RMQHelper.java
@@ -1,6 +1,5 @@
 package com.ericsson.eiffel.remrem.publish.helper;
 
-
 import com.ericsson.eiffel.remrem.publish.config.PropertiesConfig;
 import com.google.gson.JsonElement;
 import com.rabbitmq.client.AMQP.BasicProperties;
@@ -33,59 +32,67 @@ import java.util.Map.Entry;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
-@Component("rmqHelper") public class RMQHelper {
+@Component("rmqHelper")
+public class RMQHelper {
 
     private static final String FALSE = "false";
-	@Inject
-	RMQBeanConnectionFactory factory;
+    @Inject
+    RMQBeanConnectionFactory factory;
 
     private static final int CHANNEL_COUNT = 100;
     private static final Random random = new Random();
-    @Value("${rabbitmq.host}") private String host;
-    @Value("${rabbitmq.exchange.name}") private String exchangeName;
-    @Value("${rabbitmq.port}") private Integer port;
-    @Value("${rabbitmq.tls}") private String tlsVer;
-    @Value("${rabbitmq.user}") private String user;
-    @Value("${rabbitmq.password}") private String password;
-    @Value("${rabbitmq.domainId}") private String domainId;
+    @Value("${rabbitmq.host}")
+    private String host;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+    @Value("${rabbitmq.port}")
+    private Integer port;
+    @Value("${rabbitmq.tls}")
+    private String tlsVer;
+    @Value("${rabbitmq.user}")
+    private String user;
+    @Value("${rabbitmq.password}")
+    private String password;
+    @Value("${rabbitmq.domainId}")
+    private String domainId;
     private boolean usePersitance = true;
     private Connection rabbitConnection;
     private List<Channel> rabbitChannels;
-    
+
     Logger log = (Logger) LoggerFactory.getLogger(RMQHelper.class);
 
     public String getHost() {
-		return host;
-	}
+        return host;
+    }
 
-	public void setHost(String host) {
-		this.host = host;
-	}
-	
+    public void setHost(String host) {
+        this.host = host;
+    }
+
     public Integer getPort() {
-		return port;
-	}
+        return port;
+    }
 
-	public void setPort(Integer port) {
-		this.port = port;
-	}
+    public void setPort(Integer port) {
+        this.port = port;
+    }
 
-	public String getTlsVer() {
-		return tlsVer;
-	}
+    public String getTlsVer() {
+        return tlsVer;
+    }
 
-	public void setTlsVer(String tlsVer) {
-		this.tlsVer = tlsVer;
-	}
+    public void setTlsVer(String tlsVer) {
+        this.tlsVer = tlsVer;
+    }
 
-	public String getExchangeName() {
-		return exchangeName;
-	}
+    public String getExchangeName() {
+        return exchangeName;
+    }
 
-	public void setExchangeName(String exchangeName) {
-		this.exchangeName = exchangeName;
-	}
-	
+    public void setExchangeName(String exchangeName) {
+        this.exchangeName = exchangeName;
+    }
+
     public String getDomainId() {
         return domainId;
     }
@@ -94,80 +101,79 @@ import java.util.concurrent.TimeoutException;
         this.domainId = domainId;
     }
 
-	@PostConstruct public void init() {
-	    handleLogging();
+    @PostConstruct
+    public void init() {
+        handleLogging();
         log.info("RMQHelper init ...");
         initCli();
         try {
-            //ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost(host);  
+            // ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(host);
 
             if (port != null) {
-            	factory.setPort(port);
-            	log.info("Port is: " + port);
+                factory.setPort(port);
+                log.info("Port is: " + port);
             } else {
-            	log.info("Using default rabbit mq port.");
+                log.info("Using default rabbit mq port.");
             }
-            
+
             log.info("Host adress: " + host);
             log.info("Exchange is: " + exchangeName);
 
             if (tlsVer != null && !tlsVer.isEmpty()) {
                 if (tlsVer.contains("default")) {
-                	log.info("Using default TLS version connection to RabbitMQ.");
-                	factory.useSslProtocol();
+                    log.info("Using default TLS version connection to RabbitMQ.");
+                    factory.useSslProtocol();
+                } else {
+                    log.info("Using TLS version " + tlsVer + " connection to RabbitMQ.");
+                    factory.useSslProtocol("TLSv" + tlsVer);
                 }
-                else {
-                	log.info("Using TLS version " + tlsVer + " connection to RabbitMQ.");
-                	factory.useSslProtocol("TLSv" + tlsVer);
-                }
-            }
-            else{
-            	log.info("Using standard connection method to RabbitMQ.");
+            } else {
+                log.info("Using standard connection method to RabbitMQ.");
             }
 
             rabbitConnection = factory.newConnection();
-            rabbitChannels = new ArrayList<>();            
-            
+            rabbitChannels = new ArrayList<>();
+
             for (int i = 0; i < CHANNEL_COUNT; i++) {
-            	rabbitChannels.add(rabbitConnection.createChannel());
+                rabbitChannels.add(rabbitConnection.createChannel());
             }
         } catch (IOException | TimeoutException e) {
-        	log.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
         } catch (KeyManagementException e) {
-        	log.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
         } catch (NoSuchAlgorithmException e) {
-        	log.error(e.getMessage(), e);            
-		}
+            log.error(e.getMessage(), e);
+        }
     }
 
     private void initCli() {
-    	setValues();
+        setValues();
     }
-    
-    private void setValues() {
-    	String passedHost = System.getProperty(PropertiesConfig.MESSAGE_BUS_HOST); 
-    	if (passedHost != null) {
-    		host = passedHost;
-    	}
-    	
-    	Integer passedPort = Integer.getInteger(PropertiesConfig.MESSAGE_BUS_PORT); 
-    	if (passedPort != null) {
-    		port = passedPort;
-    	}
 
-    	String passedTlsVer = System.getProperty(PropertiesConfig.TLS); 
-    	if (passedTlsVer != null) {
-    		tlsVer = passedTlsVer;
-    	}
-    	
-    	String passedExchange = System.getProperty(PropertiesConfig.EXCHANGE_NAME);
-    	if (passedExchange != null) {
-    		exchangeName = passedExchange;
-    	}
-    	usePersitance = Boolean.getBoolean(PropertiesConfig.USE_PERSISTENCE);
+    private void setValues() {
+        String passedHost = System.getProperty(PropertiesConfig.MESSAGE_BUS_HOST);
+        if (passedHost != null) {
+            host = passedHost;
+        }
+
+        Integer passedPort = Integer.getInteger(PropertiesConfig.MESSAGE_BUS_PORT);
+        if (passedPort != null) {
+            port = passedPort;
+        }
+
+        String passedTlsVer = System.getProperty(PropertiesConfig.TLS);
+        if (passedTlsVer != null) {
+            tlsVer = passedTlsVer;
+        }
+
+        String passedExchange = System.getProperty(PropertiesConfig.EXCHANGE_NAME);
+        if (passedExchange != null) {
+            exchangeName = passedExchange;
+        }
+        usePersitance = Boolean.getBoolean(PropertiesConfig.USE_PERSISTENCE);
     }
-    
+
     private void handleLogging() {
         String debug = System.getProperty(PropertiesConfig.DEBUG);
         log.setLevel(Level.ALL);
@@ -180,7 +186,7 @@ import java.util.concurrent.TimeoutException;
     @PreDestroy
     public void cleanUp() throws IOException {
         log.info("RMQHelper cleanUp ...");
-        if (rabbitConnection!=null){
+        if (rabbitConnection != null) {
             rabbitConnection.close();
             rabbitConnection = null;
         } else {
@@ -215,11 +221,10 @@ import java.util.concurrent.TimeoutException;
                 exchangeName, routingKey);
     }
 
-
     private Channel giveMeRandomChannel() {
         return rabbitChannels.get(random.nextInt(rabbitChannels.size()));
     }
-    
+
     public JsonElement getInputEventId(JsonElement json) {
         if (json.isJsonObject() && json.getAsJsonObject().has(PropertiesConfig.EIFFEL_MESSAGE_VERSIONS) && json
                 .getAsJsonObject().getAsJsonObject(PropertiesConfig.EIFFEL_MESSAGE_VERSIONS).entrySet().size() > 0) {
