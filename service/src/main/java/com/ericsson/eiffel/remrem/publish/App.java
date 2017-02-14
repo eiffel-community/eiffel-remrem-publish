4ec4a23461bebe55ce03e0e6d2c4b6b3ed98d44f
UMADEVI KAPU <kapu.umadevi@tcs.com>
2017-02-14 16:01:56 +0530

    Removed version in cli and service

--- service/src/main/java/com/ericsson/eiffel/remrem/publish/App.java
+++ service/src/main/java/com/ericsson/eiffel/remrem/publish/App.java
@@ -16,26 +16,11 @@ import com.ericsson.eiffel.remrem.publish.config.SpringLoggingInitializer;
 @ComponentScan("com.ericsson.eiffel.remrem")
 public class App extends SpringBootServletInitializer {
 	 public static void main(String[] args) {
-	    	startService(args);
-	    }
-	    
-	    private static void startService(String[] args) {
-	    	handleArgs(args);
-	    	SpringApplication application = new SpringApplication(App.class);
-	    	application.addInitializers(new SpringLoggingInitializer());
-	    	application.setBannerMode(Banner.Mode.OFF);
-	    	application.setLogStartupInfo(false);
-	    	// We do not start web service if any CLI arguments are passed
-	    	application.setWebEnvironment(true);	 
-	        ApplicationContext ctx = application.run(args);
-	    }
-	    
-	    private static void handleArgs(String[] args) {
-	    	//We sort out Spring specific arguments and send the others to CLI parser
-	    	DefaultApplicationArguments springARgs = new DefaultApplicationArguments(args);
-	    	List<String> nonOptions = springARgs.getNonOptionArgs();
-	    	String[] nonSpringArgs = nonOptions.toArray(new String[0]);
-	    	// We need to parse the cLI options before Spring starts since we need the available when
-	    	// Spring instantiate autowired components needing argument values
+	     SpringApplication application = new SpringApplication(App.class);
+         application.addInitializers(new SpringLoggingInitializer());
+         application.setBannerMode(Banner.Mode.OFF);
+         application.setLogStartupInfo(false);
+         application.setWebEnvironment(true);
+         ApplicationContext ctx = application.run(args);
 	    }
 }
