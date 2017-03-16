24e35c174a7948e568f0cb9ee448bc7ac79ec8f8
xdurvak <prasad.vakapalli@tcs.com>
2017-03-16 19:24:03 +0530

    Implemented ldap authentication functionality

--- publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/App.java
+++ publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/App.java
@@ -1,26 +1,27 @@
 package com.ericsson.eiffel.remrem.publish;
 
-import java.util.List;
-
 import org.springframework.boot.Banner;
-import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 import com.ericsson.eiffel.remrem.publish.config.SpringLoggingInitializer;
 
-@SpringBootApplication  
+@SpringBootApplication
 @ComponentScan("com.ericsson.eiffel.remrem")
+@PropertySources({ @PropertySource("classpath:config.properties"),
+        @PropertySource(value = "file:${catalina.home}/conf/config.properties", ignoreResourceNotFound = true) })
 public class App extends SpringBootServletInitializer {
-	 public static void main(String[] args) {
-	     SpringApplication application = new SpringApplication(App.class);
-         application.addInitializers(new SpringLoggingInitializer());
-         application.setBannerMode(Banner.Mode.OFF);
-         application.setLogStartupInfo(false);
-         application.setWebEnvironment(true);
-         ApplicationContext ctx = application.run(args);
-	    }
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(App.class);
+        application.addInitializers(new SpringLoggingInitializer());
+        application.setBannerMode(Banner.Mode.OFF);
+        application.setLogStartupInfo(false);
+        application.setWebEnvironment(true);
+        ApplicationContext ctx = application.run(args);
+    }
 }
