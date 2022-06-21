42c0117db8e0f0ecfa818f2d55cec557596ff747
Vaishnavi Enugala <vaishnavi.enugala@ericsson.com>
2022-06-21 14:34:52 +0530

    to make build success

--- publish-service/src/test/java/com/ericsson/eiffel/remrem/publish/service/MessageServiceRMQImplUnitTest.java
+++ publish-service/src/test/java/com/ericsson/eiffel/remrem/publish/service/MessageServiceRMQImplUnitTest.java
@@ -176,16 +176,16 @@ public class MessageServiceRMQImplUnitTest {
         rmqHelper.getRabbitMqPropertiesMap().get(protocol).setWaitForConfirmsTimeOut(1L);
         rmqHelper.getRabbitMqPropertiesMap().get(protocol).init();
         JsonArray jarray = new JsonArray();
-        String body = FileUtils.readFileToString(new File("src/test/resources/MultipleValidEvents.json"));
+        String body = FileUtils.readFileToString(new File("src/test/resources/EiffelActivityFinishedEvent.json"));
         MsgService msgService = PublishUtils.getMessageService(protocol, msgServices);
-        for (int i = 1; i < 30; i++) {
+        for (int i = 1; i < 50; i++) {
             SendResult result = messageService.send(body, msgService, "test", null, null);
+            Thread.sleep(100);
             Assert.assertNotNull(result);
             for (PublishResultItem results : result.getEvents()) {
                jarray.add(results.toJsonObject());
             }
         }
-        System.out.println("the final output is........................"+jarray.toString());
         assertTrue(jarray.toString().contains("Time out waiting for ACK"));
     }
 
