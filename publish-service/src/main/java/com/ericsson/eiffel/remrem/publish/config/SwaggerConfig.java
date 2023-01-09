3527a41d33c40b8064d22c81381a5ddc58de247c
Vishnu <vishnu.alapati@ericsson.com>
2023-01-09 17:46:15 +0530

    Made changes to resolve the test failures

--- publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/config/SwaggerConfig.java
+++ publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/config/SwaggerConfig.java
@@ -14,9 +14,28 @@
 */
 package com.ericsson.eiffel.remrem.publish.config;
 
-import io.swagger.annotations.Api;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver;
+import org.springframework.boot.actuate.endpoint.web.EndpointMapping;
+import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
+import org.springframework.boot.actuate.endpoint.web.ExposableWebEndpoint;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+import io.swagger.annotations.Api;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -43,9 +62,41 @@ public class SwaggerConfig {
 
         @Bean
         public UiConfiguration uiConfig() {
-        	return new UiConfiguration(null, "list", "alpha", "schema", Constants.DEFAULT_SUBMIT_METHODS, false, true,
-    				null);
+                return new UiConfiguration(null, "list", "alpha", "schema", Constants.DEFAULT_SUBMIT_METHODS, false, true,
+                                null);
         }
+//        public UiConfiguration uiConfig() {
+//            return new UiConfiguration(false, false, null,
+//                    null, ModelRendering.of("schema"), false, DocExpansion.LIST, null, null,
+//                    OperationsSorter.ALPHA, false, false, null,
+//                    Constants.DEFAULT_SUBMIT_METHODS, null, null);
+//        }
+
+        @Bean
+        public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
+                ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
+                EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties,
+                WebEndpointProperties webEndpointProperties, Environment environment) {
+            List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+            Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+            allEndpoints.addAll(webEndpoints);
+            allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+            allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+            String basePath = webEndpointProperties.getBasePath();
+            EndpointMapping endpointMapping = new EndpointMapping(basePath);
+            boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment,
+                    basePath);
+            return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
+                    corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
+                    shouldRegisterLinksMapping, null);
+        }
+
+        private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment,
+                String basePath) {
+            return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
+                || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+        }
+        
 
         private ApiInfo metaData() {
                 final StringBuilder remremDescription = new StringBuilder();
