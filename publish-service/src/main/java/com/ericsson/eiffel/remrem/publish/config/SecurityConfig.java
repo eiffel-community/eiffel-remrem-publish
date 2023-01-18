b3a1592d93bedaeab2c30ae5be00b6c5aad98933
Vishnu <vishnu.alapati@ericsson.com>
2023-01-18 13:51:25 +0530

    Made changes to SecurityConfig class

--- publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/config/SecurityConfig.java
+++ publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/config/SecurityConfig.java
@@ -82,7 +82,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.ldapAuthentication().userSearchFilter(userSearchFilter).contextSource(ldapContextSource());
     }
 
-    @Bean
     public BaseLdapPathContextSource ldapContextSource() {
         LdapContextSource ldap = new LdapContextSource();
         ldap.setUrl(ldapUrl);
@@ -92,6 +91,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         HashMap<String, Object> environment = new HashMap<>();
         environment.put("com.sun.jndi.ldap.connect.timeout", Integer.toString(getTimeOut()));
         ldap.setBaseEnvironmentProperties(environment);
+        ldap.afterPropertiesSet();
         return ldap;
     }
 
