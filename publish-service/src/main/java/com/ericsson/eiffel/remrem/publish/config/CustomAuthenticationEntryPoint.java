ad00113dc5f036da54591bc7cbfc00e2ec48d759
Roman Szturc <roman.szturc.ext@ericsson.com>
2024-03-27 16:08:43 +0100

    Service responds HTTP status 506 in case of LDAP timeout
    EIFA-xxxxx
    
    <body>

--- publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/config/CustomAuthenticationEntryPoint.java
+++ publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/config/CustomAuthenticationEntryPoint.java
@@ -6,10 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import groovyjarjarantlr4.v4.misc.FrequencySet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -26,6 +28,17 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         if (authException instanceof BadCredentialsException) {
             LOGGER.warn("Bad Credentials {}", HttpStatus.UNAUTHORIZED);
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid credentials");
+        }
+        else if (authException instanceof InternalAuthenticationServiceException) {
+            Throwable cause = authException.getCause();
+            if (cause instanceof org.springframework.ldap.CommunicationException communicationException) {
+                cause = communicationException.getCause();
+                if (cause instanceof javax.naming.CommunicationException namingCommunicationException) {
+                    LOGGER.warn("Communication problem: {}: {}", HttpStatus.GATEWAY_TIMEOUT, namingCommunicationException.toString());
+                    response.sendError(HttpServletResponse.SC_GATEWAY_TIMEOUT, namingCommunicationException.toString());
+                    response.flushBuffer();
+                }
+            }
         }
     }
 }
