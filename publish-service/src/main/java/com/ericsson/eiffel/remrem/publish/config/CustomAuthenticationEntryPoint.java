0ec8e8ba886b0c1ccff2b6ac9aadff56e3d36d6a
z-sztrom <106370045+z-sztrom@users.noreply.github.com>
2024-04-11 08:36:57 +0200

    Service responds HTTP status 504 in case of LDAP timeout (#289)
    * Service responds HTTP status 504 in case of LDAP timeout

--- publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/config/CustomAuthenticationEntryPoint.java
+++ publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/config/CustomAuthenticationEntryPoint.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -26,6 +27,18 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         if (authException instanceof BadCredentialsException) {
             LOGGER.warn("Bad Credentials {}", HttpStatus.UNAUTHORIZED);
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid credentials");
+        }
+        else if (authException instanceof InternalAuthenticationServiceException) {
+            Throwable cause = authException.getCause();
+            if (cause instanceof org.springframework.ldap.CommunicationException communicationException) {
+                cause = communicationException.getCause();
+                if (cause instanceof javax.naming.CommunicationException namingCommunicationException) {
+                    String message = namingCommunicationException.toString();
+                    LOGGER.warn("Communication problem: {}; {}", HttpStatus.GATEWAY_TIMEOUT, message);
+                    response.sendError(HttpServletResponse.SC_GATEWAY_TIMEOUT, message);
+                    response.flushBuffer();
+                }
+            }
         }
     }
 }
