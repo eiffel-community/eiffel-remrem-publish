db41c2661c1acdd1ce9e6218247012eee18c1adf
Roman Szturc <roman.szturc.ext@ericsson.com>
2026-04-29 12:17:34 +0200

    Make /publish endpoint backward compatible
    Publish service introduced backward incompatibility when handling array
    of events in release X.Y.Z. This commit removes the incompitibility.

--- publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/controller/ProducerController.java
+++ publish-service/src/main/java/com/ericsson/eiffel/remrem/publish/controller/ProducerController.java
@@ -505,6 +505,7 @@ public class ProducerController {
             // TODO We should not rely on bodyJson (an input string), but rather on given
             //      result, i.e. response.getBody() and check this for type (object or array).
             if (bodyJson.isJsonObject()) {
+                // TODO Why array is created from a single object?
                 responseBody = "[" + response.getBody() + "]";
             } else if (bodyJson.isJsonArray()) {
                 responseBody = response.getBody();
@@ -548,6 +549,14 @@ public class ProducerController {
 
         // Status here is the status returned from generate service,
         // except BAD_REQUEST which already handled above.
+
+        if (responseEvents.size() == 1) {
+            // If one event, return its content directly instead of an array with one element.
+            // This guarantees backward compatibility.
+            return new ResponseEntity<>(responseEvents.get(0), responseStatus);
+        }
+
+        // Return list of events as a response.
         return new ResponseEntity<>(responseEvents, responseStatus);
     }
 
